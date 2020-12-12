15f6dc9f0d41c9d63282d34217a4d0f1a803b138
Ankohen <Stepanfisenko@gmail.com>
2020-12-12 19:16:28 +0300

    zadanie5

---
+++ Lesson2-master/Задание 2(5)/Задание 2(5)/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace Задание_2_5_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFileName = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.number1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.age1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnVvodData = new System.Windows.Forms.Button();
+            this.btnOpenData = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ввод имени файла из 6 латинских букв";
+            // 
+            // btnFileName
+            // 
+            this.btnFileName.Location = new System.Drawing.Point(51, 79);
+            this.btnFileName.Name = "btnFileName";
+            this.btnFileName.Size = new System.Drawing.Size(106, 23);
+            this.btnFileName.TabIndex = 2;
+            this.btnFileName.Text = "Запись в файл";
+            this.btnFileName.UseVisualStyleBackColor = true;
+            this.btnFileName.Click += new System.EventHandler(this.btnFileName_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number,
+            this.name,
+            this.age});
+            this.dataGridView1.Location = new System.Drawing.Point(276, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(374, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // number
+            // 
+            this.number.HeaderText = "№";
+            this.number.Name = "number";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            // 
+            // age
+            // 
+            this.age.HeaderText = "Age";
+            this.age.Name = "age";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number1,
+            this.name1,
+            this.age1});
+            this.dataGridView2.Location = new System.Drawing.Point(276, 270);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(374, 165);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // number1
+            // 
+            this.number1.HeaderText = "№";
+            this.number1.Name = "number1";
+            // 
+            // name1
+            // 
+            this.name1.HeaderText = "Name";
+            this.name1.Name = "name1";
+            // 
+            // age1
+            // 
+            this.age1.HeaderText = "Age";
+            this.age1.Name = "age1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 243);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 192);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Вывод записей из файла";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(701, 243);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 192);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = resources.GetString("textBox3.Text");
+            // 
+            // btnVvodData
+            // 
+            this.btnVvodData.Location = new System.Drawing.Point(831, 26);
+            this.btnVvodData.Name = "btnVvodData";
+            this.btnVvodData.Size = new System.Drawing.Size(94, 35);
+            this.btnVvodData.TabIndex = 8;
+            this.btnVvodData.Text = "Ввод данных";
+            this.btnVvodData.UseVisualStyleBackColor = true;
+            this.btnVvodData.Click += new System.EventHandler(this.btnVvodData_Click);
+            // 
+            // btnOpenData
+            // 
+            this.btnOpenData.Location = new System.Drawing.Point(820, 79);
+            this.btnOpenData.Name = "btnOpenData";
+            this.btnOpenData.Size = new System.Drawing.Size(105, 36);
+            this.btnOpenData.TabIndex = 9;
+            this.btnOpenData.Text = "Вызов из файла";
+            this.btnOpenData.UseVisualStyleBackColor = true;
+            this.btnOpenData.Click += new System.EventHandler(this.btnOpenData_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(831, 153);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 34);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(385, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Таблица 1. Ввод данных";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(372, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Таблица 2. Вывод из созданного файла";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(667, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Столбцов:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(727, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(667, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Строк:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(713, 60);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(48, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(951, 456);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpenData);
+            this.Controls.Add(this.btnVvodData);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnFileName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Задание 2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFileName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnVvodData;
+        private System.Windows.Forms.Button btnOpenData;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn age;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn age1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Lesson2-master/Задание 2(5)/Задание 2(5)/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Задание_2_5_
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()//Создание экземпляра формы
+        {
+            InitializeComponent();//Инициализация формы
+
+            
+
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+
+        //}
+
+
+        
+
+
+        public void btnFileName_Click(object sender, EventArgs e)//Кнопка создания файла с именем, которое задает пользователь
+        {
+            String Filename = textBox1.Text;
+
+            TextWriter sw = new StreamWriter(@"D:\" + Filename + ".txt");//создание файла на рабочем столе
+
+
+            try
+            {
+                //Цикл записи данных в файл из таблицы 1
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    {                      
+                        sw.Write(dataGridView1.Rows[i].Cells[j].Value.ToString() + (" "));
+                    }
+                    sw.WriteLine();
+                }
+            }
+            catch (Exception ex)//исключение, если запись не прошла
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sw.Close();//закрытие файла после записи
+            }
+
+            label9.Text = "Запись прошла успешно";
+        }
+
+
+
+
+        private void btnVvodData_Click(object sender, EventArgs e) //Кнопка завершения ввода данных
+        {
+            dataGridView1.AllowUserToAddRows = false; //Запрет на редактирование таблицы 1
+           
+            int n, m;
+
+            n = dataGridView1.Columns.Count; // показывает итоговое количество столбцов в таблице 1
+            label6.Text = n.ToString();
+
+            m = dataGridView1.Rows.Count;// показывает итоговое количество строк в таблице 1
+            label8.Text = m.ToString();
+
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)//кнопка закрытия
+        {
+            Close(); //закрывает форму
+        }
+
+        public void btnOpenData_Click(object sender, EventArgs e) //Кнопка открытия файла и перенос данных в таблицу 2 и текстбокс 2 
+        {
+
+            bool btn = true;
+            if (btn == true) //Если кнопка нажата
+            {
+                
+
+                String Filename = textBox1.Text; //Имя файла, заданное пользователем из текстбокса 1
+
+                DataTable table = new DataTable(); //Создание новой таблицы в DataGridView2
+
+
+                StreamReader rd = new StreamReader(@"D:\" + Filename + ".txt"); //Открытие файла с данными
+                string[] str;
+                int num = 0;
+
+                try //Цикл записи данных в таблицу 2 и текстбокс 2
+                {
+                    string[] str1 = rd.ReadToEnd().Split('\n'); //Прочтение всего файла построчно
+                    num = str1.Count();
+                  
+
+                    dataGridView2.RowCount = num;
+
+                    for (int i = 0; i < num; i++)
+                    {
+                        
+                            str = str1[i].Split(' ');
+                        
+                        
+                        
+                        for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                        {
+                            try
+                            {
+
+                                dataGridView2.Rows[i].Cells[j].Value = str[j];
+                            }
+
+                            catch { }
+                        }
+                    }
+                }
+                catch (Exception ex) //Исключение, если запись не удалась
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    rd.Close();//закрытие файла
+                }
+
+                dataGridView2.AllowUserToAddRows = false; //Запрет на редактирование Таблицы 2
+            }
+
+
+            textBox2_TextChanged(); //Вызов метода
+        }
+
+        public void textBox2_TextChanged() //Метод чтение текста из файла и перенос его в текст бокс 2
+        {
+            String Filename = textBox1.Text;
+            string m = File.ReadAllText(@"D:\" + Filename + ".txt"); //Чтение файла
+            
+            textBox2.Text = "№"+ " Name" + " Age" + Environment.NewLine + m; //Заполнение текстбокс 2
+
+        }
+
+
+        
+
+    }
+}
